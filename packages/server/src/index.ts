4aa4d0f535c13610f7b7bd5dea3ff11b55286aff
Andrey Platov <andrey.v.platov@gmail.com>
2020-06-18 13:15:22 +0200

    change default server port
    Signed-off-by: Andrey Platov <andrey.v.platov@gmail.com>

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -18,4 +18,4 @@ import { start } from './server'
 const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017'
 
 console.log('mongodb uri: ...' + mongodbUri.substring(25))
-start(8080, mongodbUri)
+start(18080, mongodbUri)
